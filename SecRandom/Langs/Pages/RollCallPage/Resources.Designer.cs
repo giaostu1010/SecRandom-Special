6d74d7255e57eef3adb2a86a0c8ae002cd132e9d
lzy98276 <lzy.12@foxmail.com>
2026-02-23 18:19:51 +0800

    修复冲突问题

--- SecRandom/Langs/Pages/RollCallPage/Resources.Designer.cs
+++ SecRandom/Langs/Pages/RollCallPage/Resources.Designer.cs
@@ -38,7 +38,7 @@ namespace SecRandom.Langs.Pages.RollCallPage {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SecRandom.Langs.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SecRandom.Langs.Pages.RollCallPage.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
